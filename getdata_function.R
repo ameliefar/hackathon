5c67bdcb627bae0ff3c458394f7aec2f1851ddd6
Alexandre Granier <alexandre.granier@cefe.cnrs.fr>
2025-11-25 17:02:49 +0100

    Add species and site columns

--- getdata_function.R
+++ getdata_function.R
@@ -4,8 +4,25 @@ library('tidyverse')
 
 INDIVIDUAL_DATA_PATH <- "data/individual_data.csv"
 
+SPECIES_CODE_PATH <- "data/species_codes.csv"
+
 #'Function to import dummy dataset
-getdata_dummy <- function(file = '') {
-  if(missing(file)) file <- INDIVIDUAL_DATA_PATH
-  read.table(file, sep = ",", header = T, fileEncoding = "UTF-8")
+getdata_dummy <- function() {
+  df <- readr::read_csv(INDIVIDUAL_DATA_PATH)
+  species_code <- readr::read_csv(SPECIES_CODE_PATH)
+  site_codes <- readr::read_csv("data/site_codes.csv")
+  
+  # Retirer le suffixe de studyID pour obtenir siteID
+  df <- df |>
+    mutate(siteID = sub("-.*$", "", studyID))
+  
+  # Ajouter le nom vernaculaire
+  df <- df |>
+    left_join(species_code |> select(speciesID, vernacularName), by = "speciesID")
+  
+  # Ajouter le nom du site
+  df <- df |>
+    left_join(site_codes |> select(siteID, siteName), by = "siteID")
+  
+  return(df)
 }
